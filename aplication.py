b77b855f4b471f94d383a1d586c82492b17ab9e0
Axel Eleazar Garcia Darce <darce@localhost.localdomain>
2019-04-29 23:12:07 -0600

    Agregando soporte python y flask

---
+++ aplication.py
@@ -0,0 +1,6 @@
+from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
