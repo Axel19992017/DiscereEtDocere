e4c67a7da32114d2c5f3b5ae056812caa0905dd6
Axel Eleazar Garcia Darce <darce@localhost.localdomain>
2019-04-30 15:50:59 -0600

    Haciendo que bootstrap sirva xd

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, flash, redirect, render_template, request, session
-from flask import Flask, flash, redirect, render_template, request, session
+from flask_bootstrap import Bootstrap
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
@@ -7,7 +7,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 # Configure application
 app = Flask(__name__)
-
+Bootstrap(app)
 @app.route("/")
 def index():
     return render_template("index.html")
