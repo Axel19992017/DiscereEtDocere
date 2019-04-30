2b38627b336739a21af4ed151b431fc7543810ba
Axel Eleazar Garcia Darce <darce@localhost.localdomain>
2019-04-30 00:03:03 -0600

    Corrigiendo errores al integrar Flask

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
