afa070a4fe83d961412d3b7ef693821e9a7b44fb
Axel Eleazar Garcia Darce <darce@localhost.localdomain>
2019-05-02 10:51:33 -0600

    Arreglando dolores de cabeza

--- app.py
+++ app.py
@@ -3,11 +3,19 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from flaskext.mysql import MySQL
 # Configure application
 app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PORT'] = '3306'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Axel0611.'
+app.config['MYSQL_DATABASE_DB'] = 'DED'
+mysql.init_app(app)
 @app.route("/")
 def index():
+    
     return render_template("index.html")
 
 if __name__ == '__main__':
