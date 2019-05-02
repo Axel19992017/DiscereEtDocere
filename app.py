e3ca242496f9c437e9b03dd0b9ffea8b3f1abf09
Axel Eleazar Garcia Darce <darce@localhost.localdomain>
2019-05-02 14:05:14 -0600

    Agregando toolbar

--- app.py
+++ app.py
@@ -6,17 +6,26 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from flaskext.mysql import MySQL
 # Configure application
 app = Flask(__name__)
+
 mysql = MySQL()
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-app.config['MYSQL_DATABASE_PORT'] = '3306'
+app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Axel0611.'
 app.config['MYSQL_DATABASE_DB'] = 'DED'
+
 mysql.init_app(app)
+con = mysql.connect()
+cursor = con.cursor()
+
 @app.route("/")
 def index():
-    
-    return render_template("index.html")
+    cursor.execute("select CONCAT(nombre,' ',apellido) as Nombre,alias,pase from Usuario;")
+    print(cursor.rownumber)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(f"{row}")
+    return render_template("index.html",rows = rows,rol = '')
 
 if __name__ == '__main__':
     app.run(debug=True)
