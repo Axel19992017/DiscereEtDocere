ac47231966ccfccdfcc3e626c72b334053745e81
Axel Eleazar Garcia Darce <darce@localhost.localdomain>
2019-05-03 00:01:49 -0600

    Mejorando toolbar y hacer que funcione login

--- app.py
+++ app.py
@@ -17,19 +17,48 @@ app.config['MYSQL_DATABASE_DB'] = 'DED'
 mysql.init_app(app)
 con = mysql.connect()
 cursor = con.cursor()
+rol = ''
+error = ''
+# Funciones usadas con propositos de utilidad
 
+# Si existe un Usuario con ese alias (el alias es único)
+def siExisteUsuario(alias, pase):
+    cursor.execute(f"select count(*) as Vacio from Usuario where alias = '{alias}' and pase = '{pase}';")
+    vacio = cursor.fetchone()
+    return vacio[0]
+def getRol(alias):
+    cursor.execute(f"select rol from Usuario where alias = '{alias}'")
+    rol = cursor.fetchone()
+    return rol[0]
 @app.route("/")
 def index():
     cursor.execute("select CONCAT(nombre,' ',apellido) as Nombre,alias,pase from Usuario;")
     print(cursor.rownumber)
     rows = cursor.fetchall()
-    for row in rows:
-        print(f"{row}")
-    return render_template("index.html",rows = rows,rol = "")
+    return render_template("index.html",rows = rows,rol = rol)
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    print(f'El rol es: ' + rol)
+    return render_template("login.html",error = "")
 
+@app.route("/inicioSesion", methods=['POST'])
+def inicioSesion():
+    usuario = request.form['usuario']
+    pase = request.form['pase']
+    if not usuario or not pase:
+        error = 'Datos ingresados incorrectamente'
+        return render_template("login.html", error = error)
+    elif siExisteUsuario(usuario,pase) == 0:
+        error = 'Usuario o contraseña incorrectos'
+        return render_template("login.html", error = error)
+    else:
+        rol = getRol(usuario)
+        return render_template("index.html",rol= rol)
+
+@app.route("/addUsuario")
+def addUsuario():
+    return render_template("registrarUsuario.html")
 if __name__ == '__main__':
     app.run(debug=True)
+
